5fff73840dff0ba1d7381e4e451bfad40d5a9958
John Meyer <johndmeyer@gmail.com>
2018-07-10 12:41:43 -0600

    More development
    Added Dapper and some other items. Starting to get close to having a single api functional.

--- SovosCodingExercise.DataAccess/IngredientData.cs
+++ SovosCodingExercise.DataAccess/IngredientData.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SovosCodingExercise.DataModels;
+using System.Data;
 
 namespace SovosCodingExercise.DataAccess
 {
@@ -12,6 +13,23 @@ namespace SovosCodingExercise.DataAccess
         public IEnumerable<IngredientDataModel> RetrieveIngredients()
         {
             return new List<IngredientDataModel>();
+
+
+            string sqlOrderDetails = "SELECT TOP 5 * FROM OrderDetails;";
+
+            using (var connection = new System.Data.SqlClient.SqlConnection("Data Source=SqlCe_W3Schools.sdf"))
+            {
+                var foo = connection.Query
+                //var orderDetails = connection.Query<OrderDetail>(sqlOrderDetails).ToList();
+                //var orderDetail = connection.QueryFirstOrDefault<OrderDetail>(sqlOrderDetail, new { OrderDetailID = 1 });
+                //var affectedRows = connection.Execute(sqlCustomerInsert, new { CustomerName = "Mark" });
+
+                //Console.WriteLine(orderDetails.Count);
+                //Console.WriteLine(affectedRows);
+
+                //FiddleHelper.WriteTable(orderDetails);
+                //FiddleHelper.WriteTable(new List<OrderDetail>() { orderDetail });
+            }
         }
     }
 }
